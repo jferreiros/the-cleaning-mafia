e77c4c342c6e781b7f753b35443c8ee403794d37
joaquinferreiros <jferreiros@emailingnetwork.com>
2024-02-09 11:56:22 -0500

    Fix ESLint

--- components/EmailTemplate.js
+++ components/EmailTemplate.js
@@ -2,7 +2,7 @@ export function EmailTemplate({ firstName }) {
   return (
     <div>
       <h1>Hello, {firstName}!</h1>
-      <p>Welcome to our service. We&apos;re glad you&apos;re here!</p>
+      <p>Welcome to our service. We are glad you are here!</p>
     </div>
   );
 }
